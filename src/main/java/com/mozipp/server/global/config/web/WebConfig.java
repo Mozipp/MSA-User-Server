06acdc8fa29f410e8b6ee354d58d058204ab9143
jaebinGit <tnwoql327@gmail.com>
2024-11-25 17:22:58 +0900

    Docs : web config

--- src/main/java/com/mozipp/server/global/config/web/WebConfig.java
+++ src/main/java/com/mozipp/server/global/config/web/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://api.multi-learn.com", "http://service-user-acitive.service.svc.cluster.local:8080", "http://service-auth-acitive.service.svc.cluster.local:8080", "http://service-products-acitive.service.svc.cluster.local:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
