aeab316438fc73516e9d4aa130ad94168e5b432b
so1eeee <soljeong821@gmail.com>
2024-12-05 18:33:49 +0900

    Feat : Portfolio Domain 추가

---
+++ src/main/java/com/mozipp/server/domain/portfolio/entity/Portfolio.java
@@ -0,0 +1,23 @@
+package com.mozipp.server.domain.portfolio.entity;
+
+import com.mozipp.server.domain.designer.entity.Designer;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String naverPlaceUrl;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "designer_id")
+    private Designer designer;
+}
---
+++ src/main/java/com/mozipp/server/domain/portfolio/repository/PortfolioRepository.java
@@ -0,0 +1,9 @@
+package com.mozipp.server.domain.portfolio.repository;
+
+import com.mozipp.server.domain.portfolio.entity.Portfolio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
+}
